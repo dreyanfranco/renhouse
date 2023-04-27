d87ddfa4620185924147caeb980be25784afa262
Dreyan <dreyanfranco@gmail.com>
2023-04-27 16:11:38 +0200

    general setup client

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div>
+      <header>
+        <a href='#' className=''></a>
+      </header>
+    </div>
+
+  )
+}
+
+export default App
