e55c0fc06fb658ffb7126fde22484a187691ae6b
dreyanfranco <dreyanfranco@gmail.com>
2023-06-05 12:19:18 +0200

    login done with context

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router()
 const User = require("../models/User.model")
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { isAuthenticated } = require('./../middleware/jwt.middleware');
 
 const salt = bcrypt.genSaltSync(10);
-
 
 router.get('', async (req, res) => {
   try {
@@ -29,36 +30,87 @@ router.post('/register', async (req, res) => {
   }
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login', (req, res, next) => {
 
   const { email, password } = req.body;
 
   if (email === '' || password === '') {
-    res.status(400).json({ message: "Username or password not present." });
+    res.status(400).json({ message: "Provide email and password." });
     return;
   }
 
-  try {
-    const user = await User.findOne({ email, password });
-    if (!user) {
-      res.status(401).json({
-        messaje: "Login not succesfull",
-        error: "User not found"
-      })
-    } else {
-      res.status(200).json({
-        messaje: "Login succesful",
-        user
-      })
-    }
-  } catch (error) {
-    res.status(400).json({
-      message: "An error ocurred",
-      error: error.mesagge
-    })
-  }
+  User
+    .findOne({ email })
+    .then((foundUser) => {
 
+      if (!foundUser) {
+        res.status(401).json({ message: "User not found." })
+        return;
+      }
+
+      if (bcrypt.compareSync(password, foundUser.password)) {
+
+        const { _id, email, name } = foundUser;
+
+        const payload = { _id, email, name }
+
+        const authToken = jwt.sign(
+          payload,
+          'helloworld', //secret?
+          { algorithm: 'HS256', expiresIn: "6h" }
+        )
+
+        res.status(200).json({ authToken });
+      }
+      else {
+        res.status(401).json({ message: "Unable to authenticate the user" });
+      }
+
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: "Internal Server Error" })
+    });
 });
+
+
+
+
+router.get('/verify', isAuthenticated, (req, res) => {
+  res.status(200).json(req.payload)
+})
+
+
+// router.post('/login', async (req, res) => {
+
+//   const { email, password } = req.body;
+
+//   if (email === '' || password === '') {
+//     res.status(400).json({ message: "Username or password not present." });
+//     return;
+//   }
+
+//   try {
+//     const user = await User.findOne({ email, password });
+//     if (!user) {
+//       res.status(401).json({
+//         messaje: "Login not succesfull",
+//         error: "User not found"
+//       })
+//     } else {
+//       res.status(200).json({
+//         messaje: "Login succesful",
+//         user
+//       })
+//     }
+//   } catch (error) {
+//     res.status(400).json({
+//       message: "An error ocurred",
+//       error: error.mesagge
+//     })
+//   }
+
+// });
 
 router.delete('/:user_id', async (req, res) => {
   try {
